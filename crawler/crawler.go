f4107ce4a24552fbf2b5b6446f92bf07a38b1724
thedmdim <thedmdim@gmail.com>
2022-12-07 12:49:12 +0400

    package look like, logging, error handling

---
+++ crawler/crawler.go
@@ -0,0 +1,196 @@
+package crawler
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"sync"
+
+	"github.com/alimate/unbounded-channel/channels"
+	"golang.org/x/net/html"
+)
+
+type LinksStorage struct {
+	// literally results of crawling
+	results sync.Map
+	// unique constraint to not request same link twice
+	visited sync.Map
+}
+
+type Crawler struct {
+	From string
+	Proxy string
+	MaxGoroutines int
+	Logging bool
+	// Filter string ?
+	// Connect timeout ?
+
+	jobs *channels.UnboundedChannel
+	ls *LinksStorage
+	semaphore chan bool
+}
+
+
+func (c *Crawler) Init() {
+	// Проверка переданных в Crawler параметров
+
+	if c.Logging {
+		log.SetOutput(os.Stdout)
+	} else {
+		log.SetOutput(io.Discard)
+	}
+
+	if c.Proxy != "" {
+		proxy, err := url.Parse(c.Proxy)
+		if err != nil {
+			log.Panicf("Can't read proxy address: %e", err)
+
+		}
+		http.DefaultTransport = &http.Transport{
+			Proxy: http.ProxyURL(proxy),
+		}
+	}
+
+	semaphoreDefault := 6
+	if c.MaxGoroutines == 0 {
+		log.Printf("No max goroutine provided, default %d", semaphoreDefault)
+		c.MaxGoroutines = semaphoreDefault
+	}
+
+	if c.From == "" {
+		log.Fatalf("Provide url from which we'll start crawling")
+	}
+
+	u, err := url.Parse(c.From)
+	if err != nil {
+		log.Fatalf("Cannot parse URL of %s", c.From)
+	}
+
+	c.jobs = channels.NewUnboundedChannel()
+	c.jobs.Enqueue(u.String())
+
+	c.ls = new(LinksStorage)
+	c.semaphore = make(chan bool, c.MaxGoroutines)
+}
+
+func (c *Crawler) Start() {
+	for {
+		c.semaphore <- true
+		link := c.jobs.Dequeue().(string)
+
+		go func() {
+			log.Printf("Start worker with %s", link)
+
+			results, err := Worker(link, c.ls)
+
+			if err != nil {
+				log.Printf("Worker: %v", err)
+			}
+
+			log.Printf("Found %d links from %s", len(results), link)
+
+			for _, link := range results {
+				c.jobs.Enqueue(link)
+			}
+
+			<-c.semaphore
+		}()
+	}
+}
+
+func Worker(link string, ls *LinksStorage) ([]string, error) {
+	if _, ok := ls.visited.Load(link); ok {
+		// check if we have already requsted the url
+		return nil, fmt.Errorf("%s is already visited", link)
+	}
+
+	res, err := http.Get(link)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s returned %d", link, res.StatusCode)
+	}
+
+	defer func(){
+		res.Body.Close()
+		ls.visited.Store(link, struct{}{})
+
+		u, _ := url.Parse(link)
+		if _, ok := ls.results.Load(u.Host); !ok {
+			ls.results.Store(u.Host, struct{}{})
+			// outputs result
+			fmt.Println(u.Host)
+		}
+	}()
+
+	dom, err := html.Parse(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse HTML for %s: %w", link, err)
+	}
+
+	results, err := findLinks(dom)
+	if err != nil {
+		return nil, fmt.Errorf("haven't found any link at %s", link)
+	}
+
+	relLinksToAbs(&results, link)
+	
+	// // delete from result already visited links
+	// for i:=0;i==len(results);i++{
+	// 	if _, ok := ls.visited.Load(results[i]); ok {
+	// 		results = append(results[:i], results[i+1:]...)
+	// 		i--
+	// 	}
+	// }
+
+
+	return results, nil
+
+}
+
+func findLinks(n *html.Node) ([]string, error) {
+
+	var links []string
+	// Рекурсивно ищет a[href] на странице
+
+	var f func(n *html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					links = append(links, attr.Val)
+				}
+			}
+		}
+	
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(n)
+	return links, nil
+
+	
+}
+
+func relLinksToAbs(links *[]string, baseURL string) {
+	base, _ := url.Parse(baseURL)
+
+	for i, link := range *links {
+		abs, err := base.Parse(link)
+		if err != nil {
+			continue
+		}
+
+		abs.RawQuery = "" // del get params
+		if flink := abs.String(); link != flink {
+			(*links)[i] = flink
+		}
+	
+	}
+
+}
--- main.go
+++ main.go
@@ -1,134 +1,18 @@
 package main
 
 import (
-	//"errors"
-	"fmt"
-	"log"
-	"net/http"
-	"net/url"
-	"sync"
-
-	//"time"
-
-	"github.com/alimate/unbounded-channel/channels"
-	"golang.org/x/net/html"
+	"turbo-tor-crawl/crawler"
 )
 
 func main() {
-	// set global proxy
-	proxy, err := url.Parse("socks5://127.0.0.1:9150")
-	if err != nil {
-		panic(err)
-	}
-	http.DefaultTransport = &http.Transport{
-		Proxy: http.ProxyURL(proxy),
+	c := crawler.Crawler{
+		From: "http://freshonifyfe4rmuh6qwpsexfhdrww7wnt5qmkoertwxmcuvm4woo4ad.onion/",
+		Proxy: "socks5://127.0.0.1:9150",
+		MaxGoroutines: 10,
+		//Logging: true,
 	}
 
-	jobs := channels.NewUnboundedChannel()
-	// var hosts sync.Map
-	// var visited sync.Map
-	results := new(sync.Map)
-	visited := new(sync.Map)
+	c.Init()
 
-	jobs.Enqueue("http://freshonifyfe4rmuh6qwpsexfhdrww7wnt5qmkoertwxmcuvm4woo4ad.onion")
-
-	semaphore := make(chan bool, 6)
-
-	for {
-		semaphore <- true
-		go func() {
-			worker(jobs, results, visited)
-			<-semaphore
-		}()
-	}
+	c.Start()
 }
-
-func worker (jobs *channels.UnboundedChannel, hosts *sync.Map, visited *sync.Map) {
-
-	origin := jobs.Dequeue().(string)
-
-	if _, ok := visited.Load(origin); ok {
-		// check if we have already requsted the url
-		return
-	}
-
-	res, err := http.Get(origin)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if res.StatusCode != http.StatusOK {
-		log.Printf("%s returned %d", origin, res.StatusCode)
-		return
-	}
-
-	defer func(){
-		res.Body.Close()
-		visited.Store(origin, struct{}{})
-
-		u, _ := url.Parse(origin)
-		if _, ok := hosts.Load(u.Host); !ok {
-			hosts.Store(u.Host, struct{}{})
-			fmt.Println(u.Host)
-		}
-	}()
-
-
-	dom, err := html.Parse(res.Body)
-	if err != nil {
-		return
-	}
-
-	var links []string
-	findLinks(&links, dom)
-	relLinksToAbs(&links, origin)
-	
-
-	for _, link := range links {
-		if _, ok := visited.Load(link); !ok {
-			jobs.Enqueue(link)
-		}
-	}
-	
-}
-
-func findLinks(links *[]string, n *html.Node) {
-	// Рекурсивно ищет a[href] на странице
-	if n == nil {
-		return
-	}
-
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, attr := range n.Attr {
-			if attr.Key == "href" {
-				*links = append(*links, attr.Val)
-			}
-		}
-	}
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		findLinks(links, c)
-	}
-	
-}
-
-func relLinksToAbs(links *[]string, baseURL string) {
-	base, _ := url.Parse(baseURL)
-
-	for i, link := range *links {
-		abs, err := base.Parse(link)
-		if err != nil {
-			continue
-		}
-
-		abs.RawQuery = "" // del get params
-		if flink := abs.String(); link != flink {
-			(*links)[i] = flink
-		}
-	
-	}
-
-}
-
-
-
